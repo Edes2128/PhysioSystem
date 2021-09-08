d9355162669fcaefee9513778a9ca8047114f9ea
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-09-08 12:41:36 +0200

    ASAS

--- src/pages/klient/components/Profile.jsx
+++ src/pages/klient/components/Profile.jsx
@@ -1,33 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useContext } from 'react'
 import { ReactComponent as SepTabs } from '../../../images/sep-tabs.svg'
 import { ReactComponent as HidePass } from '../../../images/hide-pass.svg'
 import { ReactComponent as Pencil } from '../../../images/pencil.svg'
+import ClientContext from '../../../context/klient/klientContext';
 
 export default function Profile() {
     const [activeTab, setActiveTab] = useState('general')
-
-    // const videoRef = useRef(null)
-    // useEffect(() => {
-    //     const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-    //     const myMediaSource = new MediaSource();
-    //     const url = URL.createObjectURL(myMediaSource);
-    //     videoRef.current.src = url;
-
-
-    //     myMediaSource.addEventListener('sourceopen', () => {
-    //         const videoSourceBuffer = myMediaSource.addSourceBuffer(mimeCodec);
-    //         fetch("https://nickdesaulniers.github.io/netfix/demo/frag_bunny.mp4").then(res => res.arrayBuffer()).then(data => {
-    //             videoSourceBuffer.appendBuffer(data)
-    //         })
-
-    //     })
-
-
-    // }, [])
+    const clientContext = useContext(ClientContext)
+    const { currentUser, setCurrentUser } = clientContext;
 
     return (
         <>
-            {/* <video id="video" className="img-res" ref={videoRef} controls ></video> */}
             <div className="profile flex fd-column ai-start" >
                 <p className="profile-title fs-38 fw-semib">Profile</p>
                 <div className="profile-tabs flex ai-center" >
@@ -56,31 +39,65 @@ export default function Profile() {
                         <div className="profile-general-inputs flex ai-center jc-spaceb">
                             <div className="profile-general-inputs-item flex fd-column ai-start">
                                 <label className="fs-20 fw-medium" htmlFor="#">Name</label>
-                                <input className="fs-20 fw-regular" type="text" />
+                                <input className="fs-20 fw-regular" type="text" value={currentUser.name}
+                                    onChange={(e) => {
+                                        setCurrentUser(prev => ({
+                                            ...prev,
+                                            name: e.target.value
+                                        }))
+                                    }} />
                             </div>
 
                             <div className="profile-general-inputs-item flex fd-column ai-start">
                                 <label className="fs-20 fw-medium" htmlFor="#">Email</label>
-                                <input className="fs-20 fw-regular" type="text" />
+                                <input className="fs-20 fw-regular" type="text" value={currentUser.email}
+                                    onChange={(e) => {
+                                        setCurrentUser(prev => ({
+                                            ...prev,
+                                            email: e.target.value
+                                        }))
+                                    }}
+                                />
                             </div>
                         </div>
 
                         <div className="profile-general-inputs flex ai-center jc-spaceb">
                             <div className="profile-general-inputs-item flex fd-column ai-start">
                                 <label className="fs-20 fw-medium" htmlFor="#">Country</label>
-                                <input className="fs-20 fw-regular" type="text" />
+                                <input className="fs-20 fw-regular" type="text" value={currentUser.country}
+                                    onChange={(e) => {
+                                        setCurrentUser(prev => ({
+                                            ...prev,
+                                            country: e.target.value
+                                        }))
+                                    }}
+                                />
                             </div>
 
                             <div className="profile-general-inputs-item flex fd-column ai-start">
                                 <label className="fs-20 fw-medium" htmlFor="#">City</label>
-                                <input className="fs-20 fw-regular" type="text" />
+                                <input className="fs-20 fw-regular" type="text" value={currentUser.city}
+                                    onChange={(e) => {
+                                        setCurrentUser(prev => ({
+                                            ...prev,
+                                            city: e.target.value
+                                        }))
+                                    }}
+                                />
                             </div>
                         </div>
 
                         <div className="profile-general-inputs flex ai-end jc-spaceb">
                             <div className="profile-general-inputs-item flex fd-column ai-start">
                                 <label className="fs-20 fw-medium" htmlFor="#">Postal Code</label>
-                                <input className="fs-20 fw-regular" type="text" />
+                                <input className="fs-20 fw-regular" type="text" value={currentUser.postal_code}
+                                    onChange={(e) => {
+                                        setCurrentUser(prev => ({
+                                            ...prev,
+                                            postal_code: e.target.value
+                                        }))
+                                    }}
+                                />
                             </div>
 
                             <div className="profile-general-inputs-item flex fd-column ai-start">
