ad96b801c609d7c215e01e660fca9f4ec060edc6
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-09-13 11:07:44 +0200

    ASA

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Register from './components/Register';
 import Klient from './pages/klient/Klient';
 import Fizioterapist from './pages/fizioterapist/Fizioterapist';
 import KlientProvider from './context/klient/KlientProvider';
+import AlertState from './context/alerts/AlertState';
+import Alerts from './components/Alerts';
 
 function App() {
   const path = useLocation();
@@ -19,13 +21,16 @@ function App() {
 
   return (
     <>
-    <KlientProvider>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/shop" component={Klient} />
-        <Route path="/fizio" component={Fizioterapist} />
-      </Switch>
+      <KlientProvider>
+        <AlertState>
+          <Alerts />
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/shop" component={Klient} />
+            <Route path="/fizio" component={Fizioterapist} />
+          </Switch>
+        </AlertState>
       </KlientProvider>
     </>
   );
--- src/pages/klient/components/Orders.jsx
+++ src/pages/klient/components/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ReactComponent as Search } from '../../../images/loupe.svg'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -13,9 +13,17 @@ export default function Orders() {
 
     const clientContext = useContext(ClientContext)
     const { getMyPackages, mypackages } = clientContext;
+    const [page, setPage] = useState(1);
+    const [itemPage, setItempage] = useState(10);
+    const start = (page - 1) * itemPage;
+    const end = page * itemPage;
     useEffect(() => {
         getMyPackages()
     }, [])
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
     return (
         <div className="orders" >
             <div className="orders-header flex ai-center jc-spaceb">
@@ -39,7 +47,7 @@ export default function Orders() {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {mypackages && mypackages.map((item, index) => (
+                        {mypackages && mypackages.slice(start, end).map((item, index) => (
                             <TableRow>
                                 <TableCell>#{index + 1}</TableCell>
                                 <TableCell>{item.packages[0].name}</TableCell>
@@ -53,7 +61,7 @@ export default function Orders() {
             </div>
 
             <div className="pagination flex jc-end">
-                <Pagination count={mypackages.length} />
+                <Pagination count={Math.ceil(mypackages.length / itemPage)} size="medium" onChange={handleChange} />
             </div>
 
         </div>
