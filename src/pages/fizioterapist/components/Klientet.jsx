029de22f1bfba22cc9ab133d19e57e9770d0d055
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-09-15 13:11:31 +0200

    ASAS

--- src/pages/fizioterapist/components/Klientet.jsx
+++ src/pages/fizioterapist/components/Klientet.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { ReactComponent as Search } from '../../../images/loupe.svg'
 
 export default function Klientet() {
     return (
-        <div>
+        <div className="klientet flex fd-column ai-start">
             
+            <div className="klientet-top flex ai-center jc-spaceb">
+                <p className="klientet-top-title fs-30 fw-medium" >Klientet <sup  className="fs-20">(20)</sup></p>
+                <div className="header-search flex ai-center">
+                    <Search />
+                    <input className="fs-16 fw-regular" type="text" placeholder="Search..." />
+                </div>
+            </div>
+
+            <div className="klientet-datatable">
+                
+            </div>
+
+
         </div>
     )
 }
