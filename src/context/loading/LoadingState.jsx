2ab60afd994844d09609a0a1b9c568c4464e67f8
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-10-01 17:51:38 +0200

    ASAS

--- src/context/loading/LoadingState.jsx
+++ src/context/loading/LoadingState.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import LoadingContext from './LoadingContext'
 export default function LoadingState({ children }) {
     const [show, setShow] = useState(false);
+    const [collapse, setCollapse] = useState(false)
     return (
-        <LoadingContext.Provider value={{ show, setShow }} >
+        <LoadingContext.Provider value={{ show, setShow, setCollapse, collapse }} >
             {children}
         </LoadingContext.Provider>
     )
