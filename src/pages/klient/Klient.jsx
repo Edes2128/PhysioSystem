8e6f1b126015bdea3874f512d0f31dfa4292bd2d
Edes2128 <51410696+Edes2128@users.noreply.github.com>
2021-10-06 18:32:56 +0200

    SAS

--- src/pages/klient/Klient.jsx
+++ src/pages/klient/Klient.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 import Body from './components/Body'
 import HaderBanner from './components/HaderBanner'
-
-
+import HeaderMobile from './components/HeaderMobile'
 export default function Klient({ history }) {
-
     const logout = () => {
         history.push('/')
         localStorage.removeItem('token')
         localStorage.removeItem('op')
     }
-
-
     return (
         <div className="klient" >
             <Sidebar logout={logout} />
             <Body />
             <HaderBanner />
+            <HeaderMobile />
         </div>
     )
 }
--- src/pages/klient/components/HeaderMobile.jsx
+++ src/pages/klient/components/HeaderMobile.jsx
@@ -4,11 +4,10 @@ import { ReactComponent as Cart } from '../../../images/cart-2.svg'
 import { ReactComponent as MenuIcon } from '../../../images/icons8-menu-orange.svg'
 import ClientContext from '../../../context/klient/klientContext'
 import LoadingContext from '../../../context/loading/LoadingContext'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
 import MiniCart from './MiniCart'
 
-export default function HaderBanner() {
+export default function HeaderMobile() {
     const collapseContext = useContext(LoadingContext)
     const { setCollapse } = collapseContext
     const clientContext = useContext(ClientContext);
@@ -22,10 +21,9 @@ export default function HaderBanner() {
         })
         getCart()
     }, [])
-
     return (
-        <div className="headerbanner flex fd-column ai-center" >
-            <div className="headerbanner-top flex ai-center jc-center">
+        <div className="header-mobile" >
+            <div className="headerbanner-top flex ai-center jc-start">
                 <MenuIcon onClick={() => setCollapse(true)} className="headerbanner-top-ham-menu" />
                 <div className="headerbanner-top-left flex ai-center">
                     <div className="headerbanner-top-left-widget flex ai-center jc-center"  >
@@ -45,23 +43,6 @@ export default function HaderBanner() {
                         <img src={currentUser && `https://physiosystem.alcodeit.com/files/${currentUser.image_profile}`} className="img-res" alt="" />
                     </div>
                     <p className="fs-16 fw-medium" >{currentUser && currentUser.name}</p>
-                </div>
-            </div>
-
-            <div className="headerbanner-offers flex fd-column ai-start">
-                <p className="headerbanner-offers-title fs-32 fw-bold">Offers</p>
-                <p className="headerbanner-offers-subtitle fs-16 fw-regular">Get a look at our best offers</p>
-                <div className="headerbanner-offers-content" >
-                {offers.length !== 0 && offers.map(oferta => (
-                    <Link to={`/shop/offer/${oferta.id}`} className="headerbanner-offers-item flex">
-                        <div className="headerbanner-offers-item-shape"></div>
-                        <div className="headerbanner-offers-item-details">
-                            <p className="fs-16 fw-regular" >{oferta.titulli_ofertes}</p>
-                            <span className="fs-32 fw-semib" > - {oferta.ulja}  {oferta.ulja_type === 1 ? '%' : '$'} </span>
-                        </div>
-                        <img src={`https://physiosystem.alcodeit.com/files/${oferta.baner_oferta}`} className="img-res" alt="" />
-                    </Link>
-                ))}
                 </div>
             </div>
         </div>
